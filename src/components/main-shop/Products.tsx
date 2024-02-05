ed547807f8bc7b2f61d9d3dd41b614ef13f30b63
andryxatt <vlasandrey878@gmail.com>
2024-02-05 14:12:32 +0200

    add changes filters

--- src/components/main-shop/Products.tsx
+++ src/components/main-shop/Products.tsx
@@ -5,16 +5,11 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../../store/slices/product.slice";
 import { AppDispatch } from "../../store/store";
-import { loadMoreProduct, setActiveFilters } from "../../store/slices/blacklist.slice";
-import { useLocation } from "react-router-dom";
-import { FaFilter } from "react-icons/fa6";
+import { loadMoreProduct } from "../../store/slices/blacklist.slice";
 import { BsFillGrid3X3GapFill, BsFillGridFill } from "react-icons/bs";
 import MainLayout from "../../layouts/MainLayout";
 import ActiveBar from "./ActiveBar";
-import Breadcrumb from "./Breadcrumb";
 const Products = () => {
-  const location = useLocation();
-  const isActiveFilter = useAppSelector((state) => state.blackListSlice.isActive);
   const products = useAppSelector((state) => state.productSlice.products);
   const limit = useAppSelector((state) => state.blackListSlice.limit);
   const filters = useAppSelector((state) => state.blackListSlice.filters);
